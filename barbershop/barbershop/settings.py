ebc8d245ff6474ae1289764a6d50bf7cd9928307
Ефимов Александр <efimov_aleksandr@ws-k1.class-15.real-it>
2024-02-14 13:47:43 +0500

    new

--- barbershop/barbershop/settings.py
+++ barbershop/barbershop/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'price_list'
+    # 'users',
+    'price_list',
 ]
 
 MIDDLEWARE = [
--- barbershop/price_list/urls.py
+++ barbershop/price_list/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from barbershop.price_list import views
+from . import views
 
 urlpatterns = [
-    path('/', views.index, name = 'home'),
+    path('', views.index, name='home'),
 ]

93d581c605d4fa65cfe7065cd70e36c0566e1e23
Aleksandr <aleksandr_almaz@mail.ru>
2024-02-20 20:04:27 +0500

    от 20.02.2024

--- barbershop/users/models.py
+++ barbershop/users/models.py
@@ -1,12 +1,13 @@
+from datetime import timezone
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class CustomUser(AbstractUser):
-    telephone_number = models.BigIntegerField(db_index = True,
-            unique = True, verbose_name = "Номер телефона")
-    photo = models.ImageField(upload_to = "users/%Y/%m/%d/", blank = True, 
-            null = True, verbose_name = "Фотография")
-    birthday = models.DateTimeField(blank = True, null = True, 
-            verbose_name = "Дата рождения")
+def update_last_login(sender, user, **kwargs):
+    """
+    A signal receiver which updates the last_login date for
+    the user logging in.
+    """
+    user.last_login = timezone.now()
+    user.save(update_fields=["last_login"])

3bb1840840d4a8486e4fc1d5a94ff8fe78613e91
Aleksandr <aleksandr_almaz@mail.ru>
2024-02-24 15:21:54 +0500

    error add Customers-2

--- barbershop/price_list/models.py
+++ barbershop/price_list/models.py
@@ -4,20 +4,48 @@ from django.urls import reverse
 # Create your models here.
 
 
+# Категории видов предоставляемых услуг
+class Category(models.Model):
+    category_name = models.CharField(max_length = 100, verbose_name="Категория")
+    category_slug = models.SlugField(max_length = 250, db_index = True, unique = True, verbose_name="Слаг")
+    category_content = models.TextField(blank = True, verbose_name = "Описание категории")
+
+    class Meta:
+        verbose_name = 'Категорию'
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.category_name
+    
+
+# Виды клиентов
+class Customers(models.Model):
+    customers_name = models.CharField(max_length = 100, verbose_name="Категория клиента")
+    customers_slug = models.SlugField(max_length = 250, db_index = True, unique = True, verbose_name ="Слаг")
+    customers_content = models.TextField(blank = True, verbose_name = "Описание")
+
+    class Meta:
+        verbose_name = 'Категория клиента'
+        verbose_name_plural = "Категории клиентов"
+
+    def __str__(self):
+        return self.customers_name
+
+
 # Прайс
 class Price(models.Model):
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, 
-        related_name='Category', verbose_name = 'Категории')
-    customers = models.ForeignKey('Customers', on_delete=models.PROTECT, 
-        related_name='Customers', verbose_name = "Категория клиента")
-    title = models.CharField(max_length = 250, blank = False, verbose_name = "Заголовок")
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, 
+        related_name='category', verbose_name = 'Категории')
+    is_customers = models.ForeignKey(Customers, null=True, default=None, 
+        on_delete=models.CASCADE, related_name='customers', verbose_name = "Категория клиента")
+    title = models.CharField(max_length = 250, default = True, blank = True, verbose_name = "Заголовок")
     slug = models.SlugField(max_length = 250, unique = True, db_index = True, verbose_name = "Slug",
         validators = [
             MinLengthValidator(5, message = "Минимум 5 символов"),
             MaxLengthValidator(250, message = "Максимум 250 символов")
         ])
     content = models.TextField(blank = True, verbose_name = "Описание")
-    is_price = models.IntegerField(blank = False, null=True, verbose_name = "Цена")
+    is_price = models.IntegerField(blank = True, null=True, verbose_name = "Цена")
     published = models.BooleanField(default = True, verbose_name = "Опубликовать")
     not_published = models.BooleanField(default = False, verbose_name = "Снять с публикации")
     photo = models.ImageField(upload_to = "photos/%Y/%m/%d", default = None, blank = True, 
@@ -33,33 +61,6 @@ class Price(models.Model):
     def __str__(self):
         return self.title
 
-
-# Категории видов предоставляемых услуг
-class Category(models.Model):
-    category_name = models.CharField(max_length = 100, db_index = True, verbose_name="Категория")
-    category_slug = models.SlugField(max_length = 250, unique = True, db_index = True, verbose_name="Слаг")
-    category_content = models.TextField(blank = True, verbose_name = "Описание категории")
-
-    class Meta:
-        verbose_name = 'Категорию'
-        verbose_name_plural = "Категории"
-
-    def __str__(self):
-        return self.category_name
-    
-
-# Виды клиентов
-class Customers(models.Model):
-    customers_name = models.CharField(max_length = 100, db_index = True, verbose_name="Категория клиента")
-    customers_slug = models.SlugField(max_length = 250, unique = True, db_index = True, verbose_name ="Слаг")
-    customers_content = models.TextField(blank = True, verbose_name = "Описание")
-
-    class Meta:
-        verbose_name = 'Категория клиента'
-        verbose_name_plural = "Категории клиентов"
-
-    def __str__(self):
-        return self.customers_name
 
 
 # Акции
@@ -95,7 +96,8 @@ class Stock(models.Model):
 """
 for i in Price.objects.all():
     print('Заголовок: ', i.title, 'содержание: ', i.content, 'Слаг: ', i.slug)
-  """
+
 
 for i in Stock.objects.all():
     print('Заголовок: ', i.stock_title,", ", "Слаг: ", i.slug, ", ", "Описание: ", i.stock_content,)
+"""
